fd322bfa4a99e04fc79581a38464d76b4ca279af
Franz Heinfling <franz.heinfling@gmail.com>
2014-11-23 14:00:42 +0100

    refactor

--- go/server/main.go
+++ go/server/main.go
@@ -1,23 +1,16 @@
 package feedme
 
 import (
-        "html/template"
         "net/http"
         "time"
         "fmt"
-        // "os"
-    // "io"
-        // "io/ioutil"
-
         "appengine"
         "appengine/datastore"
-        // "github.com/iand/feedparser"
-
-    "appengine/urlfetch"
-    "github.com/alexjlockwood/gcm"
-    "encoding/json"
-    "io/ioutil"
-    "strings"
+        "appengine/urlfetch"
+        "github.com/alexjlockwood/gcm"
+        "encoding/json"
+        "io/ioutil"
+        "strings"
 )
 
 // [START greeting_struct]
@@ -62,85 +55,16 @@ func deviceKey(c appengine.Context) *datastore.Key {
     return datastore.NewKey(c, "Device", "default_device", 0, nil)
 }
 
-// guestbookKey returns the key used for all guestbook entries.
-func guestbookKey(c appengine.Context) *datastore.Key {
-        // The string "default_guestbook" here could be varied to have multiple guestbooks.
-        return datastore.NewKey(c, "Guestbook", "default_guestbook", 0, nil)
-}
-
-// [START func_root]
 func root(w http.ResponseWriter, r *http.Request) {
 
-        // resp, err := http.Get("http://www.feedforall.com/sample.xml")
-
-        // if err == nil {
-        //     fmt.Fprint(w, resp.Body)
-        // } else {
-        //     fmt.Fprint(w, "blergh!")
-        // }
-
-    // fe := feedparser.Feed{"asdf", "asdf", "http://www.feedforall.com/sample.xml", nil}
-    // res, err := http.NewRequest("GET", "http://www.feedforall.com/sample.xml", nil)
-
-    // if err == nil {
-    //     feed := feedparser.NewFeed(res.Body)
-    //     fmt.Fprint(w, feed)
-    // }
-
-        // if err != nil {
-        //     fmt.Fprint(w, "error: " + err)
-        //     os.Exit(1)
-        // } else {
-        //     Feed.NewFeed(ioutil.ReadAll(response.Body))
-        // }
-
-        c := appengine.NewContext(r)
-        // Ancestor queries, as shown here, are strongly consistent with the High
-        // Replication Datastore. Queries that span entity groups are eventually
-        // consistent. If we omitted the .Ancestor from this query there would be
-        // a slight chance that Greeting that had just been written would not
-        // show up in a query.
-        // [START query]
-        q := datastore.NewQuery("Greeting").Ancestor(guestbookKey(c)).Order("-Date").Limit(10)
-        // [END query]
-        // [START getall]
-        greetings := make([]Greeting, 0, 10)
-        if _, err := q.GetAll(c, &greetings); err != nil {
-                http.Error(w, err.Error(), http.StatusInternalServerError)
-                return
-        }
-        // [END getall]
-        if err := guestbookTemplate.Execute(w, greetings); err != nil {
-                http.Error(w, err.Error(), http.StatusInternalServerError)
-        }
 }
-// [END func_root]
 
-var guestbookTemplate = template.Must(template.New("book").Parse(`
-<html>
-  <head>
-    <title>Go Guestbook</title>
-  </head>
-  <body>
-    {{range .}}
-      {{with .Author}}
-        <p><b>{{.}}</b> wrote:</p>
-      {{else}}
-        <p>An anonymous person wrote:</p>
-      {{end}}
-      <pre>{{.Content}}</pre>
-    {{end}}
-    <form action="/sign" method="post">
-      <div><textarea name="content" rows="3" cols="60"></textarea></div>
-      <div><input type="submit" value="Sign Guestbook"></div>
-    </form>
-  </body>
-</html>
-`))
+
 
 func register(w http.ResponseWriter, r *http.Request) {
     if r.Method == "POST" {
         c := appengine.NewContext(r)
+
         fu := Device{
             DeviceToken: r.Header.Get("deviceToken"),
             // Feeds: r.Header.
@@ -164,7 +88,6 @@ func register(w http.ResponseWriter, r *http.Request) {
                     http.Error(w, err.Error(), http.StatusInternalServerError)
                     return
                 }
-
                 break
             }
 
